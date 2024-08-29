655021a02cbbe7dbb1984c23e78ab7b1d7c3f368
AnandhuAuzone <118871949+AnandhuAuzone@users.noreply.github.com>
2024-08-29 11:08:49 +0530

    form changed and footer added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthResponseData, AuthService } from '../auth.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
@@ -11,23 +10,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private authService : AuthService,private router : Router ) { }
-  userForm = new FormGroup({
-    userName : new FormControl (''),
-    passWord :  new FormControl (''),
-  });
+  userForm!: FormGroup;
+  submitted = false;
+  isLoginMode = false;
+  title : string = '';
+  error : string = '';
+  constructor(private authService : AuthService,private router : Router,private formBuilder: FormBuilder) { }
   ngOnInit(): void {
+    this.title = 'In';
+    this.userForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email,Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+  });
   }
+
+
+  get f() { return this.userForm.controls; }
+
+
   onSubmit(){
-    console.log(this.userForm.value.userName)
-    console.log(this.userForm.value.passWord)
-    let user : any = this.userForm.value.userName;
-    let passWord : any = this.userForm.value.passWord;
-    let authObs : Observable<AuthResponseData>;
-    authObs =  this.authService.login(user,passWord);
-    authObs.subscribe((res) => {
-      this.router.navigate(['/home'])
-    })
+    if(this.userForm.valid){
+      this.submitted = true;
+      let user : any = this.userForm.value.email;
+      let passWord : any = this.userForm.value.password;
+      let authObs : Observable<AuthResponseData>;
+      if(this.isLoginMode == false){
+        authObs =  this.authService.login(user,passWord);
+      }else{
+        authObs = this.authService.signUp(user,passWord);
+      }
+      authObs.subscribe((res) => {
+        this.router.navigate(['/home'])
+      },(error) => {
+        this.error = error;
+      })
+    }
+  }
+
+  onSignUp(){
+    this.title = this.isLoginMode == true ? 'In' : 'Up';
+    this.isLoginMode =  !this.isLoginMode;
+    console.log(this.isLoginMode)
   }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ShortenPipe } from './shorten.pipe';
+import { FooterComponent } from './footer/footer.component';
 
 
 
@@ -9,13 +10,15 @@ import { ShortenPipe } from './shorten.pipe';
   declarations: [
     NavBarComponent,
     ShortenPipe,
+    FooterComponent,
   ],
   imports: [
     CommonModule
   ],
   exports:[
     ShortenPipe,
-    NavBarComponent
+    NavBarComponent,
+    FooterComponent
   ],
 })
 export class SharedModule { }
